e3d359bf487ae3ad8cf7592b379f16c3a96c728a
qw167 <qw167@DESKTOP-EE6OHHM>
2020-12-11 12:47:09 +0900

    오전 커밋

--- src/test/java/kr/or/member/MainController.java
+++ src/test/java/kr/or/member/MainController.java
@@ -1,16 +1,28 @@
 package kr.or.member;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.TimeInfo;
+
 import kr.or.member.MemberService;
 import kr.or.member.MemberVO;
 
 public class MainController {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// 실행 메서드
 		MemberVO memberVO = new MemberVO();
-		memberVO.setName("홍길동");
-		memberVO.setAge(45);
-		memberVO.setPhoneNum("000-0000-0000");
+		memberVO.setName("박정수");
+		memberVO.setAge(27);
+		memberVO.setPhoneNum("010-3308-0000");
 		MemberVO memberVO2 = new MemberVO();
 		memberVO2.setName("성춘향");
 		memberVO2.setAge(18);
@@ -27,7 +39,30 @@ public class MainController {
 		// 전에 사용했던 출력보다는 개선된 방식으로 출력(아래) MemberService 서비스 전용 클래스만들엇 처리.
 		MemberService memberService = new MemberService();// 자바 오브젝트 객체 생성하는 방법
 		memberService.printMember(members);// 서비스클래스의 메서드 호출
-
+		
+		//서버시간 가져오기 코딩(아래)
+		//서버시간을 가져올 수 있는 commons-net 메이븐 디펜던시 사용 라이브러리를 추가.
+		NTPUDPClient timeClient = new NTPUDPClient();
+		timeClient.setDefaultTimeout(1000);
+		//네트워크로 서버시간을 가져올때 접속하는 응답이 1초를 넘기면 재접속해라.
+		timeClient.open();
+		//http://pool.ntp.org/는 서버시간보내줄 실제 서버 주소.
+		InetAddress address = InetAddress.getByName("pool.ntp.org");
+		TimeInfo timeInfo = timeClient.getTime(address);
+		//서버시간을 담은 timeinfo 객체변수를 사용(아래) 변수는 카멜표기법*
+		//TimeStamp 1970년부터 초단위로 계산된 현재까지의 초를 합친결과값(아래) 48321574982초
+		long returnTime =  timeInfo.getMessage().getTransmitTimeStamp().getTime();
+		//위 반환받은 타임스탬프초 값을 사람이 알아볼수 있는 시간으로 변환(아래)
+		Date nowDate = new Date(returnTime);
+		//System.out.println("현재 서버시간은-영어버전  " + nowDate);
+		//Wed Dec 09 11:18:04 KST 2020 -> 2020-12-09 11:26:05 AM
+		//SimpleDateFormat formatDate = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		//System.out.println("현재 서버시간은-한국버전" + formatDate.format(nowDate));
+		LocalDateTime localDateTime = nowDate.toInstant()
+				.atZone(ZoneId.systemDefault())
+				.toLocalDateTime();
+		System.out.println("Server 시간: " + localDateTime);
+		System.out.println("로컬PC 시간: " + localDateTime.now());
 	}
 
 }
---
+++ src/test/java/kr/or/test/Week.java
@@ -0,0 +1,10 @@
+package kr.or.test;
+/**
+ * 
+ * @author 박정수
+ *
+ */
+public enum Week {
+	월,화,수,목,금,토,일
+
+}
