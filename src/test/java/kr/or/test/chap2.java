8838bb18a8d337814835346a6bfe6b2cfb04f4b6
qw167 <qw167@DESKTOP-EE6OHHM>
2021-02-01 09:50:28 +0900

    커밋

--- src/test/java/kr/or/test/chap2.java
+++ src/test/java/kr/or/test/chap2.java
@@ -3,8 +3,15 @@ package kr.or.test;
 public class chap2 {
 
 	public static void main(String[] args) {
-		for(int i = 2; i<= 9; i++ ) {
-			
+		int[][] intArray = new int[4][4];
+		int num = 1;
+		for(int i=0; i<intArray.length; i++) {
+			for(int j=0; j<intArray[i].length; j++) {
+				intArray[i][j] = num++;
+				System.out.print(intArray[i][j]+ "\t");
+			}System.out.println();
 		}
+		
+			
 	}
 }
